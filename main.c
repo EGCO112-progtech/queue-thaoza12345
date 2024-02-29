e4feb7c46b44e5ea087811536395ee25c993f8b7
thaoza12345 <156444362+thaoza12345@users.noreply.github.com>
2024-02-29 14:36:43 +0000

    .

--- main.c
+++ main.c
@@ -6,14 +6,14 @@
 #include "Queue.h"
 
 int main(int argc , char **argv) {
-  NodePtr headPtr=NULL;
-   NodePtr tailPtr=NULL;
+  NodePtr headPtr = NULL;
+  NodePtr tailPtr = NULL;
 /* For struct Queue*/
   Queue  q;
-  q. headPtr=NULL;
-  q.tailPtr=NULL;
-  q.size=0;
-  int i,x;
+  q.headPtr = NULL;
+  q.tailPtr = NULL;
+  q.size = 0;
+  int i, x;
   
   // enqueue_struct(&q, 2);
   // enqueue_struct(&q, 3);
@@ -30,18 +30,21 @@ int main(int argc , char **argv) {
   // printf("%d\n", dequeue_struct(&q));
   // printf("%d\n", dequeue_struct(&q));
   // printf("%d\n", dequeue_struct(&q));
- for(i=1;i<argc;i++){
-        if(strcmp(argv[i],"x")==0){
-            x=dequeue_struct(&q);
-            if (x != 0)
-              printf("dequeing %d\n",x);
-            else
-              printf("Empty queue\n");
-        }
-        else {
-       enqueue_struct(&q, atoi(argv[i]));
-           
-        }
+ for(i = 1; i < argc; i++)
+ {
+  if(strcmp(argv[i], "x") == 0)
+  {
+    x = dequeue_struct(&q);
+    if (x == 0)
+      printf("Empty queue\n");
+  }
+  else 
+  {
+    enqueue_struct(&q, atoi(argv[i]), atoi(argv[i+1]));
+    i += 1; // มีไว้เพื่อข้ามไป 2 เลขเพราะเลขแรกจะเก็บตัวเลือกอาหารส่วนเลขที่สองจะเก็บจำนวนที่สั่ง เช่น 3 4 สั่งอาหารที่ 3 จำนวน 4 ที่
+  }
  }
+ printf("================================================\n");
+ printf("There are %d people left in the queue\n", q.size); //บอกจำนวน order ที่ยังไม่ได้ทำ
   return 0;
 }
